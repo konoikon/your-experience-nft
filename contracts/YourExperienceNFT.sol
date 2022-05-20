114c4c57dfae42f4587e331992760ba69ec43e1d
Konstantinos Oikonomou <konstantinos.oikonomou@vanderbilt.edu>
2022-05-20 18:14:06 -0400

    starter code

---
+++ contracts/YourExperienceNFT.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+import {Base64} from "../libraries/Base64.sol";
+
+contract YourExperienceNFT is ERC721URIStorage, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("Your Experience NFT", "YE") {}
+
+  function mintNFT(
+    string memory _name,
+    string memory _description,
+    string memory _image
+  ) public {
+    uint256 newItemId = _tokenIds.current();
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "My ',
+            _name,
+            ' experience", "description": "This NFT testifies my experience at ',
+            _name,
+            ". ",
+            _description,
+            '", "image": "',
+            _image,
+            '"}'
+          )
+        )
+      )
+    );
+
+    string memory finalTokenUri = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    console.log("\n--------------------");
+    console.log(finalTokenUri);
+    console.log("--------------------\n");
+
+    _safeMint(msg.sender, newItemId);
+
+    _setTokenURI(newItemId, finalTokenUri);
+
+    _tokenIds.increment();
+  }
+}
